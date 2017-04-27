5518382d1280da30f4b300a6de7fc046d4346b82
strabox <pardal.pires@gmail.com>
2017-04-27 23:21:10 +0100

    fix

--- script.sh
+++ script.sh
@@ -39,7 +39,11 @@ then
 elif [ $task = 2 ] || [ $task = 3 ] || [ $task = 4 ]	#Clean and run
 then
 	cleanTempDirectory
-	export CLASSPATH="$CLASSPATH:./src/main/lib/aws-java-sdk-1.11.123/*:./src/main/lib/BIT:./src/main/java/metrics:./src/main/java/raytracer:./src/main/java/raytracer/pigments:./src/main/java/raytracer/shapes:./src/main/java/webserver:./src/main/java/loadbalancer:./src/main/java:./bin/BIT:./bin/metrics:./bin/raytracer/:./bin/raytracer/pigments/:./bin/raytracer/shapes:./bin/webserver:./bin/loadbalancer:../src/main/lib/aws-java-sdk-1.11.123/*"
+	export CLASSPATH="$CLASSPATH:$(find ./src/main/java -type d | tr '\n' ':' | sed 's/:$//')"
+	export CLASSPATH="$CLASSPATH:./src/main/lib/BIT"
+	export CLASSPATH="$CLASSPATH:./bin/BIT"
+	export CLASSPATH="$CLASSPATH:./src/main/lib/aws-java-sdk-1.11.123/*"
+
 	export _JAVA_OPTIONS="-XX:-UseSplitVerifier "$_JAVA_OPTIONS
 	mkdir $temp > /dev/null 2>&1	#Make temp directory where put binaries
 	echo "[SCRIPT]COMPILING CODE..."
