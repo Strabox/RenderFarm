25fa69bd965f8a86135b223005664e83d6779475
filipe <filipe.n.costa@tecnico.ulisboa.pt>
2017-05-09 14:53:33 +0100

    Load balance

--- src/main/java/renderfarm/loadbalancer/loadbalancing/BestLoadBalancing.java
+++ src/main/java/renderfarm/loadbalancer/loadbalancing/BestLoadBalancing.java
@@ -12,14 +12,57 @@ import renderfarm.loadbalancer.Request;
  */
 public final class BestLoadBalancing extends LoadBalancing {
 
+	private static final MAXIMUM_LOAD=8;
+
 	public BestLoadBalancing(AmazonDynamoDB dynamoDB) {
 		super(dynamoDB);
 	}
 	
 	@Override
 	public RenderFarmInstance getFitestMachineAlgorithm(RenderFarmInstanceManager im, Request req) {
-		// TODO Our final balancing algorithm runs here
-		throw new RuntimeException("Algorithm not implemented");
+		List<RenderFarmInstance> currentInstances = im.getCurrentInstances();
+		RenderFarmInstance previous_instance=null;
+		synchronized(currentInstances) {
+			if(currentInstances.empty() || currentInstances.get(0).getLoadLevel()+req.getWeight()>MAXIMUM_LOAD){
+				//CRIO INSTANCIA
+				//return da nova
+				return null;
+			}
+			else{
+				for(RenderFarmInstance instance : currentInstances){
+					if(instance.getLoadLevel()+req.getWeight()>MAXIMUM_LOAD){
+						return previous_instance;
+					}
+					previous_instance=instance;	
+				}
+				return instance;
+			}		
+		}
+
+
+
+
+
+
+
+
+
+
+		synchronized(currentInstances) {
+			for(RenderFarmInstance instance : currentInstances){
+				DescribeInstancesRequest describeInstancesRequest = new DescribeInstancesRequest();
+	       	 	describeInstancesRequest.withInstanceIds(instance.getId());
+	       	 	DescribeInstancesResult res = im.getAmazonEC2().describeInstances(describeInstancesRequest);
+	       	 	InstanceState state = res.getReservations().get(0).getInstances().get(0).getState();
+	       	 	if(state.getCode() == RenderFarmInstance.RUNNING){
+	       	 		if(instance.getIp() == null){
+	       	 			instance.setIp(res.getReservations().get(0).getInstances().get(0).getPublicIpAddress());
+	       	 		}
+	       	 		return instance;
+	       	 	} 
+			}
+			throw new NoInstancesToHandleRequest();
+		}
 	}
 
 
