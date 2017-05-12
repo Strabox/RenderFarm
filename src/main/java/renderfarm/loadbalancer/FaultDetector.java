ec54382a4ac11c13cdf71bca654fe029a2fbaf3c
Strabox <pardal.pires@gmail.com>
2017-05-12 21:46:45 +0100

    ad

---
+++ src/main/java/renderfarm/loadbalancer/FaultDetector.java
@@ -0,0 +1,13 @@
+package renderfarm.loadbalancer;
+
+/**
+ * Used to detect faults in render farms and notify threads that
+ * are connected to them.
+ * @author Andre
+ *
+ */
+public class FaultDetector {
+
+	
+	
+}
